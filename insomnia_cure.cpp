4e0c7a9ddb0ac3910e4eff288471f8b37bdcd28c
muhaiminverse <muhaiminverse@gmail.com>
2023-08-01 11:18:41 +0600

    2nd

---
+++ insomnia_cure.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int k,l,m,n,d;
+
+    cin>>k>>l>>m>>n>>d;
+
+    int sz = d+1;
+    int c = 0;
+
+    vector<int> a(sz);
+    vector<int> b = {k,l,m,n}; 
+
+    for (int i = 1; i <sz; i++)
+        a[i]=-1;
+    
+    for(int x : b){
+        
+        for (int i = x; i < sz; i+=x)
+                if(a[i] != 0){
+                       a[i]=0;
+                }
+    }
+    
+    for (int i = 1; i <sz; i++)
+        if(a[i]==-1)    c++;
+
+    cout<<(d-c);
+}
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    for (int i = 2; i <= 24; i+=2)
+    {
+        cout<<i<<" ";
+    }
+    
+}
